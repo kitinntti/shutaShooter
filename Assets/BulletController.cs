1f955b10ba013b511607c9d321385c5d0e07e521
kitinntti614 <kitinntti614@gmail.com>
2019-05-20 22:00:42 +0200

    サイトのやつの状態

---
+++ Assets/BulletController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+  public GameObject explosionPrefab;
+
+	void Update () {
+		transform.Translate (0, 0.2f, 0);
+
+		if (transform.position.y > 5) {
+			Destroy (gameObject);
+		}
+	}
+  void OnTriggerEnter2D(Collider2D coll){
+    GameObject.Find ("Canvas").GetComponent<UIController>().AddScore();
+
+    GameObject effect = Instantiate (explosionPrefab, transform.position, Quaternion.identity) as GameObject;
+    Destroy (effect, 1.0f);
+    Destroy (coll.gameObject);
+    Destroy (gameObject);
+  }
+}
---
+++ Assets/RockGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockGenerator : MonoBehaviour {
+
+	public GameObject rockPrefab;
+
+	void Start () {
+		InvokeRepeating ("GenRock", 1, 1);
+	}
+
+	void GenRock () {
+		Instantiate (rockPrefab, new Vector3 (-5.5f + 10 * Random.value, 6, 0), Quaternion.identity);
+	}
+}
