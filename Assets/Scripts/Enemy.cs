f377db9c45cdfbf4c72620c1c9698346a3b6c987
kitinntti614 <kitinntti614@gmail.com>
2019-05-21 23:29:55 +0200

    チュートリアル部分完成

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,12 +6,18 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     private Transform playerPos;
+    private Player player;
+    public GameObject effect;//はじけるエフェクト
 
     void Start(){
-      playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+      playerPos = GameObject.FindGameObjectWithTag("Player").transform;
       //Playerというタグのついたゲームオブジェクトを探し出す。(GameObject.FindGameObjectWithTag("Player"))
-      //その上でそのオブジェクトの位置を取得する(GetComponent<Transform>())
+      //その上でそのオブジェクトの位置を取得する(.transform())
       //つまりplayerPosはプレーヤーの場所に関する変数。
+      player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+      //Playerというタグのついたゲームオブジェクトを探し出す。(GameObject.FindGameObjectWithTag("Player"))
+      //その上で"Playerについているスクリプト（コンポネントスクリプト)を取得する (そこにHealthの情報があるから、
+      //「敵と接触するとプレーヤーがダメージを受ける」機能を付ける際には、呼び出さないといけない)
     }
 
     void Update()
@@ -20,5 +26,22 @@ public class Enemy : MonoBehaviour
         //enemyをplayerPosに向けて動かす = (Vector2.MoveTowards)。
         //現在のポジション(transform.position)からプレーヤーの場所(playerPos.position)に、一定のスピード(speed * Time.deltaTime)で向かわせる。
         //speedはpublic floatなのでコンポネント上で自由な値に設定できる。
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+      if(other.CompareTag("Player")){
+        player.health--;
+        Instantiate(effect, transform.position, Quaternion.identity);//敵がプレーヤーに接触すると敵に「はじけるエフェクト」発動
+        Destroy(gameObject);
+        //Enemyがplayerに接触すると、Playerのhealthが1減る。
+        //プレーヤーに接触した敵は消滅(Destroy)する。
+      }
+      if(other.CompareTag("Projectile")){
+        Instantiate(effect, transform.position, Quaternion.identity); //敵が玉に接触すると敵に「はじけるエフェクト」発動
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+        //弾(projectile)とEnemyが接触したら、Enemyが消える。
+        //球(other.gameObject)も消える
+      }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float speed;
+    public int health = 10;
+    //プレーヤーの体力。とりあえず10に設定。
 
+    public Text healthDisplay;
+    //テキストで体力(healthDisplay)を表示する。
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
 
@@ -21,6 +27,15 @@ public class Player : MonoBehaviour
         //moveInputで縦横のインプットを取得
         //moveVelocityでインプットに対してどれだけ動くか、すなわちspeedを設定する.
         //speedはpublic floatだからコンポネントで10にでも10000にでも好きに調整できる。
+
+        healthDisplay.text = "HEALTH :" + health;
+        //これで「HealthDisplay」に現在のhealthが表示される。
+
+        if(health <= 0){
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+          //プレーヤーの体力が0以下になったら、シーンをリロードする。
+        }
+
     }
 
     void FixedUpdate(){
@@ -31,4 +46,5 @@ public class Player : MonoBehaviour
       //フレームレート60の機器では1秒間に右に60m動くがフレームレート30の機器では1秒間に右に30mしか動かない。
       //この問題をなくすためにフレームレートに関わらず動かせるTime.DeltaTimeを使う。
     }
+
 }
